00caf0c0c99dcd45c1c46313521d964c0d82386c
Pedro Adelino <pedro.mello@virtus.ufcg.edu.br>
2017-11-01 13:52:22 -0300

    Class about generic canDeactivate

--- rotas/src/app/alunos/aluno-form/aluno-form.component.ts
+++ rotas/src/app/alunos/aluno-form/aluno-form.component.ts
@@ -1,3 +1,4 @@
+import { IFormCanDeactivate } from './../../guards/iform-canactivate';
 import { AlunosService } from './../alunos.service';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs/RX';
@@ -8,7 +9,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
   templateUrl: './aluno-form.component.html',
   styleUrls: ['./aluno-form.component.css']
 })
-export class AlunoFormComponent implements OnInit, OnDestroy {
+export class AlunoFormComponent implements OnInit, OnDestroy, IFormCanDeactivate {
 
   aluno: any = {};
   inscricao: Subscription
--- rotas/src/app/guards/alunos-deactivate.guard.ts
+++ rotas/src/app/guards/alunos-deactivate.guard.ts
@@ -1,16 +1,17 @@
+import { IFormCanDeactivate } from './iform-canactivate';
 import { Observable } from 'rxjs/Rx';
 import { AlunoFormComponent } from './../alunos/aluno-form/aluno-form.component';
 import { Injectable } from '@angular/core';
 import { CanDeactivate, ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
 
 @Injectable()
-export class AlunosDeactivateGuard implements CanDeactivate<AlunoFormComponent> {
+export class AlunosDeactivateGuard implements CanDeactivate<IFormCanDeactivate> {
         canDeactivate(
-            component: AlunoFormComponent,
+            component: IFormCanDeactivate,
             route: ActivatedRouteSnapshot,
             state: RouterStateSnapshot
         ): Observable<boolean>|Promise<boolean>|boolean {
             console.log('Guarda de Desativação');
-            return component.podeDesativar ? component.podeDesativar() : true;
+            return component.podeDesativar();
     }
 }
